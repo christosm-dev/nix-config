7ff44a4ffa3622422f8ddbd3d3e647bdc1be9473
Christos <christosm-dev@users.noreply.github.com>
2026-04-19 17:10:16 +0100

    Configure static IP for reliable headless boot on nixbox

--- hosts/nixbox/configuration.nix
+++ hosts/nixbox/configuration.nix
@@ -16,7 +16,14 @@
 
   # Networking
   networking.hostName = "nixbox";
-  networking.networkmanager.enable = true;
+  networking.useDHCP = false;
+  # Static IP - reliable headless boot without DHCP negotiation delay
+  networking.interfaces.enp1s0.ipv4.addresses = [{
+    address = "192.168.0.24";
+    prefixLength = 24;
+  }];
+  networking.defaultGateway = "192.168.0.1";
+  networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
 
   # Timezone and locale
   time.timeZone = "Europe/London";
