ec4cca51adb0ce16c19ebd2b2b8d4df80353146b
Christos <christosm-dev@users.noreply.github.com>
2026-04-29 20:32:18 +0100

    No suspend

--- hosts/nixbox/configuration.nix
+++ hosts/nixbox/configuration.nix
@@ -28,6 +28,11 @@
   # which can block boot when no display is connected
   systemd.services.NetworkManager-wait-online.enable = false;
 
+  systemd.sleep.extraConfig = ''
+    AllowSuspend=no
+    AllowHibernation=no
+  '';
+
   # Timezone and locale
   time.timeZone = "Europe/London";
   i18n.defaultLocale = "en_GB.UTF-8";
