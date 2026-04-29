537e63e07e458730e773747b7b44c99226131a73
Christos <christosm-dev@users.noreply.github.com>
2026-04-29 20:02:08 +0100

    Added PipeWire to Nixbox

--- hosts/nixbox/configuration.nix
+++ hosts/nixbox/configuration.nix
@@ -113,4 +113,28 @@
   system.stateVersion = "24.11";
 
   hardware.logitech.wireless.enable = true;
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    wireplumber.enable = true;
+  };
+
+  # Disable PulseAudio if it was enabled
+  hardware.pulseaudio.enable = false;
+
+  # Enable Bluetooth HFP
+  services.pipewire.wireplumber.extraConfig = {
+    "bluetooth" = {
+      "monitor.bluez.properties" = {
+        "bluez5.enable-sbc-xq" = true;
+        "bluez5.enable-msbc" = true;
+        "bluez5.enable-hw-volume" = true;
+        "bluez5.headset-roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
+      };
+    };
+  };
+
 }
